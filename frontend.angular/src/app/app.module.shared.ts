dc82ff8e45eb2de843e275ed0604615fecb4e520
Henrik Binggl <henrik.binggl@gmail.com>
2019-10-27 16:12:28 +0100

    feat: allign UI to mydms

--- frontend.angular/src/app/app.module.shared.ts
+++ frontend.angular/src/app/app.module.shared.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { MatProgressSpinnerModule, MatSnackBarModule, MatTooltipModule } from '@angular/material';
-// import { MatChipsModule } from '@angular/material/chips';
 // import { MatCardModule } from '@angular/material/card';
 // import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
 import { AppComponent } from './components/app/app.component';
 import { EditComponent } from './components/edit/edit.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { EllipsisPipe } from './shared/pipes/ellipsis';
-import { ApiUserService } from './shared/service/api.sites.service';
 import { ApiAppInfoService } from './shared/service/api.app.service';
+import { ApiUserService } from './shared/service/api.sites.service';
 import { ApplicationState } from './shared/service/application.state';
 
 
 @NgModule({
-  imports: [ MatProgressSpinnerModule, MatTooltipModule, MatSnackBarModule, MatButtonModule ],
-  exports: [ MatProgressSpinnerModule, MatTooltipModule, MatSnackBarModule, MatButtonModule ],
+  imports: [ MatProgressSpinnerModule, MatTooltipModule, MatSnackBarModule, MatButtonModule, MatChipsModule ],
+  exports: [ MatProgressSpinnerModule, MatTooltipModule, MatSnackBarModule, MatButtonModule, MatChipsModule ],
 })
 export class AppMaterialModule { }
 
